d548e74820c4f3e64f63a17836f28f0e44981cb0
Andrés Cidel <ac@manoderecha.mx>
2015-05-15 18:52:07 -0500

    Fix scripts

--- create-host.sh
+++ create-host.sh
@@ -4,11 +4,12 @@ while getopts "c:" opt; do
   case $opt in
     c)
       if [ ! -z "$2" ]; then
-	      printf "Password:"
+              printf "Password:"
         read -s passw
         [ ! -d "/home/ubuntu/sites/${2}" ] && mkdir -p "/home/ubuntu/sites/${2}"
-        docker run -d --name "${2}-nginx" -e HOSTNAME="${2}.frontendvo.com" -v "/home/ubuntu/sites/${2}":/var/www/ nginx
-        docker exec -d ubuntu-ssh ['/root/bin/cnu', '${2}', '$passw']
+        docker run -d --name "${2}-nginx" -e VIRTUAL_HOST="${2}.frontendvo.com" -v "/home/ubuntu/sites/${2}":/var/www/html/website dockermd/nginx
+        echo "Created: ${2}.frontendvo.com"
+        docker exec -d sshd /root/bin/cnu ${2} $passw
       else
         >&2 echo "Error: d option requires two arguments."
         exit 1
